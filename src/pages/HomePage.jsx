16902ccf4851926dd9915059727a6f2f0ed1b150
Samuel <61295615+boatengsamueltuga@users.noreply.github.com>
2026-05-11 13:50:17 -0500

    Setup React frontend architecture with Redux and Tailwind

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div className="text-3xl font-bold p-10">
+            SolydShop Frontend Started
+        </div>
+    );
+};
+
+export default HomePage;
